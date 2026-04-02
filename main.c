a2c56cea63fb9c5c831178360e0786169f260c96
gunderbus <cole.gundermann@gmail.com>
2026-04-01 22:42:16 -0400

    ok

--- main.c
+++ main.c
@@ -2,23 +2,5 @@
 #include <stdlib.h>
 
 int main() {
-    // Open a file for reading (ASCII text)
-    FILE *fp = fopen("example.txt", "r");
-    if (fp == NULL) {
-        printf("Error opening file!\n");
-        return 1;
-    }
-
-    // Check the file pointer is valid
-    if (fp != NULL) {
-        // Read a block of data from the file
-        char buffer[1024];
-        size_t bytesRead = fread(buffer, 1, 1024, fp);
-        printf("Read %zu bytes: '%s'\n", bytesRead, buffer);
-
-        // Close the file
-        fclose(fp);
-    }
-
-    return 0;
+    printf("nigga \n");
 }
