80aa2dceedf69d2151f45b323d10004ee3f5025d
gunderbus <cole.gundermann@gmail.com>
2026-04-01 22:36:49 -0400

    did vector2D

--- Vector.c
+++ Vector.c
@@ -1,13 +1,14 @@
 #include "Vector2D.h"
 
 int getX(Vector2D *v){
-    
+    return v->x;
 }
 
 int getY(Vector2D *v){
-    
+    return v->y;
 }
 
 void setPosition(Vector2D *v, int x, int y){
-    
+    v->x = x;
+    v->y = y;
 }
