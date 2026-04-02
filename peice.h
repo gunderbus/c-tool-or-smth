1992f97e2767f2a9282e15ab2e0d60d7ba8ac10f
gunderbus <cole.gundermann@gmail.com>
2026-04-01 22:50:29 -0400

    high matenence

--- peice.h
+++ peice.h
@@ -1,12 +1,19 @@
 #ifndef PEICE_H
 #define PEICE_H
 
+#import "Vector2D.h"
+#import "Vector.c"
+
 typedef struct peice{
     char *name;
-    int *pos;
-    int *givenMotions;
+    Vector2D *pos;
+    Vector2D *givenMotions;
     char *image;
 } peice;
 
+peice createPeice(char name, Vector2D pos, Vector2D *givenMotions, char *image);
+int destroyPeice(peice *WhichPeice);
+Vector2D getPos(peice *WhichOne);
+Vector2D *getGivenMotions(peice *WhichOne);
 
 #endif
