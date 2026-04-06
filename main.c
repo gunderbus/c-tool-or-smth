d7f9b8cad461a6bd398d6eabaa6ddd04590962f9
gunderbus <cole.gundermann@gmail.com>
2026-04-05 23:52:13 -0400

    diddy

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char board[8][8] = {"||||||||", "||||||||", "||||||||", "||||||||", "||||||||", "||||||||", "||||||||", "||||||||", };
+
+int placePiece(int posx, int posy, char given) {
+    if(board[posx][posy] != "|"){
+        return -1;
+    }
+
+    board[posx][posy] = given;
+
+    return 0;
+}
+
+int printBoard(){
+    for(int i = 0; i < 8; i++){
+        printf(board[i]);
+    }
+}
+
 int main() {
-    printf("nigga \n");
+    printBoard();
 }
